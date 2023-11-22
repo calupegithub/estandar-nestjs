0ed757121de84fbd1a9dec84672a936d3e2ae371
calupegithub <calupe@gmail.com>
2023-11-22 16:09:44 -0500

    Inicio Configuracion dinamica del DataSource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,15 @@ import { Module } from '@nestjs/common';
 import { TareasModule } from './tareas/tareas.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeormConfig } from './config/typeorm.config';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeormConfig), TareasModule],
+  imports: [
+    ConfigModule.forRoot({}),
+    TypeOrmModule.forRoot(typeormConfig),
+    TareasModule,
+    DatabaseModule,
+  ],
 })
 export class AppModule {}
